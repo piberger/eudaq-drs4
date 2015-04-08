fa5f3ea488140e75f0ddf450e45325d3515acc9b
Felix Bachmair <bachmair@phys.ethz.ch>
2015-04-08 15:14:02 +0200

    removed zero plane array, added getWaveformGraph

--- monitors/onlinemon/include/WaveformHistos.hh
+++ monitors/onlinemon/include/WaveformHistos.hh
@@ -12,13 +12,15 @@
 #include <TH1F.h>
 #include <TFile.h>
 #include <TString.h>
-
+#include <TGraph.h>
 //std
 #include <map>
 #include <algorithm>    // std::min_element, std::max_element
 #include <cstdlib>
 //eudaq
 #include "SimpleStandardEvent.hh"
+#include "TGraphSet.hh"
+
 
 class RootMonitor;
 
@@ -27,7 +29,7 @@ class WaveformHistos {
     std::string _sensor;
     int _id;
     bool _wait;
-//    std::vector<TGraph*> _Waveforms;
+    std::vector<TGraph*> _Waveforms;
     int _n_wfs;
     TH1F* h_minVoltage;
     TH1F* h_maxVoltage;
@@ -36,24 +38,22 @@ class WaveformHistos {
   public:
     WaveformHistos(SimpleStandardWaveform p, RootMonitor * mon);
     virtual ~WaveformHistos(){}
-    void Fill(const SimpleStandardWaveform & plane);
+    void Fill(const SimpleStandardWaveform & wf);
     void Reset();
 
     void Calculate(const int currentEventNum);
     void Write();
-    unsigned GetNWaveforms() const {return _n_wfs;}
-//    TGraph * getWaveformGraph(int i) { return _Waveforms[i%_n_wfs]; }
+    unsigned GetNWaveforms() const {return _n_wfs;};
+    TGraph * getWaveformGraph(int i) { return _Waveforms[i%_n_wfs]; }
     void setRootMonitor(RootMonitor *mon)  {_mon = mon; };
     TH1F* getDeltaVoltageHisto() const { return h_deltaVoltage;};
     TH1F* getMinVoltageHisto() const { return h_minVoltage;};
     TH1F* getMaxVoltageHisto() const { return h_maxVoltage;};
     TH1F* getFullIntegralVoltageHisto() const { return h_FullIntegral;};
 
-
   private:
+    unsigned int n_fills;
     void InitHistos();
-    int ** plane_map_array; //store an array representing the map
-    int zero_plane_array(); // fill array with zeros;
     int SetHistoAxisLabelx(TH1* histo,std::string xlabel);
     int SetHistoAxisLabely(TH1* histo,std::string ylabel);
     int SetHistoAxisLabels(TH1* histo,std::string xlabel, std::string ylabel);
--- monitors/onlinemon/src/WaveformHistos.cc
+++ monitors/onlinemon/src/WaveformHistos.cc
@@ -9,7 +9,7 @@
 #include "OnlineMon.hh"
 
 
-WaveformHistos::WaveformHistos(SimpleStandardWaveform p, RootMonitor * mon): _n_wfs(10),_sensor(p.getName()), _id(p.getID())
+WaveformHistos::WaveformHistos(SimpleStandardWaveform p, RootMonitor * mon): _n_wfs(10),_sensor(p.getName()), _id(p.getID()),n_fills(0)
 {
 	char out[1024], out2[1024];
 	_mon=mon;
@@ -34,18 +34,12 @@ void WaveformHistos::InitHistos() {
 	hName = TString::Format("h_FullIntegral_%s_%d",_sensor.c_str(),_id);
 	hTitle = TString::Format("%s %d: FullIntegral",_sensor.c_str(),_id);
 	h_FullIntegral = new TH1F(hName,hTitle,2000,minVolt,maxVolt);
-}
-
-int WaveformHistos::zero_plane_array()
-{
-	//  for (int i=0; i<_maxX; i++)
-	//  {
-	//    for (int j=0; j< _maxY; j++)
-	//    {
-	//      plane_map_array[i][j]=0;
-	//    }
-	//  }
-	return 0;
+	for (int i = 0; i < _n_wfs; i++){
+		_Waveforms.push_back(new TGraph());
+		_Waveforms.back()->Draw("A");
+		_Waveforms.back()->GetXaxis()->SetTitle("n");
+		_Waveforms.back()->GetYaxis()->SetTitle("Signal / mV");
+	}
 }
 
 void WaveformHistos::Fill(const SimpleStandardWaveform & wf)
@@ -58,17 +52,29 @@ void WaveformHistos::Fill(const SimpleStandardWaveform & wf)
 	h_minVoltage->Fill(min);
 	h_maxVoltage->Fill(max);
 	h_deltaVoltage->Fill(delta);
+//	cout<<"Name: "<<wf.getName()<<" ID: "<<wf.getID()<<endl;
+	TString name = TString::Format("wf_%s_%d_%d",wf.getName().c_str(),wf.getID());
+	TGraph* gr = _Waveforms[n_fills%_n_wfs];
+	cout<<"Set Data in Graph "<<n_fills%_n_wfs<<endl;
+	for (int n = 0; n < wf.getNSamples();n++)
+		gr->SetPoint(n,n,wf.getData()[n]);
+	if (n_fills==0){
+		gr->Draw("APL");
+		gr->GetXaxis()->SetTitle("n");
+		gr->GetYaxis()->SetTitle("Signal / mV");
+	}
+	n_fills++;
 }
 
 void WaveformHistos::Reset() {
 	//	for (int i = 0; i < _Waveforms.size(); i++)
 	//		_Waveforms[i]->Reset();
 	// we have to reset the aux array as well
-	zero_plane_array();
 	h_FullIntegral->Reset();
 	h_minVoltage->Reset();
 	h_maxVoltage->Reset();
 	h_deltaVoltage->Reset();
+	n_fills = 0;
 }
 
 void WaveformHistos::Calculate(const int currentEventNum)
