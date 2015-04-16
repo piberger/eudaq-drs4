f2b03aef4cf509e3f41bfbee14b6336e736b7b57
Felix Bachmair <bachmair@phys.ethz.ch>
2015-04-16 11:15:48 +0200

    added WaveformOptions Class

---
+++ monitors/onlinemon/include/WaveformOptions.hh
@@ -0,0 +1,17 @@
+/*
+ * WaveformOptions.hh
+ *
+ *  Created on: Apr 16, 2015
+ *      Author: bachmair
+ */
+
+#ifndef WAVEFORMOPTIONS_HH_
+#define WAVEFORMOPTIONS_HH_
+
+class WaveformOptions {
+public:
+	WaveformOptions();
+	virtual ~WaveformOptions();
+};
+
+#endif /* WAVEFORMOPTIONS_HH_ */
---
+++ monitors/onlinemon/src/WaveformOptions.cc
@@ -0,0 +1,18 @@
+/*
+ * WaveformOptions.cc
+ *
+ *  Created on: Apr 16, 2015
+ *      Author: bachmair
+ */
+
+#include <WaveformOptions.hh>
+
+WaveformOptions::WaveformOptions() {
+	// TODO Auto-generated constructor stub
+
+}
+
+WaveformOptions::~WaveformOptions() {
+	// TODO Auto-generated destructor stub
+}
+
