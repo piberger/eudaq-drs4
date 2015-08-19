67ba30d12345623fbe31e0ddaa4a5ff0b8b9ee97
Felix Bachmair <bachmair@phys.ethz.ch>
2015-08-19 21:16:05 +0200

    added split function

--- main/include/eudaq/CMSPixelHelper.hh
+++ main/include/eudaq/CMSPixelHelper.hh
@@ -32,7 +32,23 @@ using namespace pxar;
 
 namespace eudaq {
 
+
+std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
+  std::stringstream ss(s);
+  std::string item;
+  while (std::getline(ss, item, delim)) {
+    elems.push_back(item);
+  }
+  return elems;
 }
+
+
+std::vector<std::string> split(const std::string &s, char delim) {
+  std::vector<std::string> elems;
+  split(s, delim, elems);
+  return elems;
+}
+
     struct VCALDict {
         int row;
         int col;
