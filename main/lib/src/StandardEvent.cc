3c0282ad9107ae0832a70e1aa91ee143f0d1a83a
micha <micha.reichmann@gmail.com>
2016-04-21 08:14:47 +0200

    fixing getIntegral method

--- main/lib/src/StandardEvent.cc
+++ main/lib/src/StandardEvent.cc
@@ -63,23 +63,25 @@ float StandardWaveform::getIntegral(uint16_t low_bin, uint16_t high_bin, uint16_
 	if (high_bin > this->GetNSamples() - 1) high_bin = uint16_t(this->GetNSamples() - 1);
 	float max_low_length = (peak_pos - low_bin) * float(.5);
 	float max_high_length = (high_bin - peak_pos - 1) * float(.5);
-    uint16_t size = std::min(m_samples.size(), tcal->size());
+    uint16_t size = uint16_t(std::min(m_samples.size(), tcal->size()));
 	float integral = tcal->at((tcell + peak_pos) % size) * m_samples.at(peak_pos);  // take the value at the peak pos as start value
 	// sum up the times if the bins to the left side of the peak pos until max length is reached
 	uint16_t i = uint16_t(peak_pos - 1);
-	float low_length = tcal->at((peak_pos + tcell) % size) / 2.;
+	float low_length = tcal->at((peak_pos + tcell) % size) / float(2.);
 	while (low_length + tcal->at((i + tcell) % size) < max_low_length) {
 		low_length += tcal->at((i + tcell) % size);
 		integral += m_samples.at(i) * tcal->at((i + tcell) % size);
+		if (i < 1) break;
 		i--;
 	}
 	integral += (max_low_length - low_length) * m_samples.at(i);
 	// same thing for the right side
 	i = uint16_t(peak_pos + 1);
-	float high_length = tcal->at((peak_pos + tcell) % size) / 2.;
+	float high_length = tcal->at((peak_pos + tcell) % size) / float(2.);
 	while (high_length+tcal->at((i + tcell) % size) < max_high_length) {
 		high_length += tcal->at((i + tcell) % size);
 		integral += m_samples.at(i) * tcal->at((i + tcell) % size);
+		if (i > size - 2) break;
 		i++;
 	}
 	integral += (max_high_length - high_length) * m_samples.at(i);
